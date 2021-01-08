e1cca551438ed900ada9baf7e358b582209b870d
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-08 15:57:16 +0100

    Added useeffect on first render

--- src/BlogList.js
+++ src/BlogList.js
@@ -8,7 +8,7 @@ const BlogList = ({blogs, title, handleDelete}) => {
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
-                <div className="blog-preview" key="blog.id">
+                <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <button onClick={() => handleDelete(blog.id)}>Delete blog post</button>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -18,6 +18,10 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    useEffect(() => {
+        console.log('changes');
+    }, []);
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
@@ -26,7 +30,6 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>            
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" handleDelete={handleDelete}/>            
         </div>
     );
 }
@@ -38,3 +41,5 @@ export default Home;
 //const handleClickAttr = (name) => {
 //  console.log(name);
 //}   
+
+//<BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" handleDelete={handleDelete}/>            
