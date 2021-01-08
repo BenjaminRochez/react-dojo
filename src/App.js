17ed3d82a7121d4c7389c331be751be94f78b191
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-08 15:30:57 +0100

    filter blog post with prop function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from './Navbar.js';
 import Home from './Home';
 // NOTE: if before 17 -> import React from 'react'
 function App() {
-  const title = 'Welcome world';
+  //const title = 'Welcome world';
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -10,17 +10,23 @@ const Home = () => {
         console.log(name);
     }*/
 
+    
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" />            
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" />            
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>            
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" handleDelete={handleDelete}/>            
         </div>
     );
 }
